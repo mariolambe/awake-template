80a5a4bd2503ee3e0d94d29a37b66614848d4c27
mariolambe <61537859+mariolambe@users.noreply.github.com>
2021-04-21 21:59:46 +0200

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'Mario Lambertucci',
+  tagline: 'SEO Expert',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
