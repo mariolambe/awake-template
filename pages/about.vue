98d26c8739bd421c81222308f7936747b04c84b1
mariolambe <61537859+mariolambe@users.noreply.github.com>
2021-04-22 15:07:05 +0200

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -8,13 +8,18 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-         <h3>Fake Heading</h3>
          <p>
           
-          Hi, my name is Mario Lambertucci!
-          I have 6+ years’ experience in SEO and currently I am <b>Senior SEO Manager</b> at Zalando SE in Germany 🇩🇪
-          I’ve worked on the organic expansion and the definition of SEO requirements of 18+ domains at Zalando.
+          Hi, my name is Mario Lambertucci!<br>
+          I have 6+ years’ experience in SEO and currently I am <b>Senior SEO Manager</b> at Zalando SE in Germany<br>
+          I’ve worked on the organic expansion and the definition of SEO requirements of 18+ domains at Zalando.<br>
           Strong focus on strategies to increase rankings, from technical SEO to link acquisition and content strategies in line with company’s goals.
+          </p>
+          <h3>Contact me</h3>
+         <p>
+          
+         If you need SEO support for your company or your online project feel free to reach out to me:<br>
+         <b>mariolambertucci@gmail.com</b>
           </p>
         </div>
       </template>
