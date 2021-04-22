64d8c988cdfe45c48a51211a384f782491795754
mariolambe <61537859+mariolambe@users.noreply.github.com>
2021-04-22 15:31:39 +0200

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -32,6 +32,21 @@
     </main-section>
   </div>
 </template>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
 <script>
 import QuoteCard from '~/components/cards/QuoteCard'
 export default {
